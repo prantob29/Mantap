d052e1b4f710c0107a588aa20f2516e3c1f54a63
prantob29 <107422370+prantob29@users.noreply.github.com>
2023-02-11 03:30:03 +0600

    Update slhost.sh

--- ssh/slhost.sh
+++ ssh/slhost.sh
@@ -21,9 +21,9 @@ echo "$SUB_DOMAIN" >> /etc/v2ray/domain
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 DOMAIN=mantapxsl.my.id
-SUB_DOMAIN=onichan-${sub}.mantapxsl.my.id
-NS_DOMAIN=zerosl-${sub}.mantapxsl.my.id
-CF_ID=slinfinity69@gmail.com
+SUB_DOMAIN=onichan-${sub}.test2.sshcloud.live
+NS_DOMAIN=zerosl-${sub}.test2.sshcloud.live
+CF_ID=prantob29@gmail.com
 CF_KEY=dd2c5e0313f122b3c1833471d469b1025f492
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
