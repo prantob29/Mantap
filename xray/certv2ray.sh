fe022eab80887e39b6bb7f2f9c1fd21468121649
prantob29 <107422370+prantob29@users.noreply.github.com>
2023-02-11 22:31:40 +0600

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -23,7 +23,7 @@ bash acme.sh --install
 rm acme.sh
 cd .acme.sh
 echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
-bash acme.sh --register-account -m kimochilol@gmail.com
-bash acme.sh --issue --standalone -d $domain --force
-bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+bash acme.sh --register-account -m prantousa@gmail.com
+bash acme.sh --issue --standalone -d sub0.sshcloud.live --force
+bash acme.sh --installcert -d sub0.sshcloud.live --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
 
